9512778106dd0bae139ce20e5c6dbbc5186d31f9
Piyal Biswas <biswaspiyal777@gmail.com>
2020-05-10 08:20:33 +0600

    double and triple equals confusion cleared

---
+++ double-vs-triple-equals.js
@@ -0,0 +1,21 @@
+const first=2;
+const second="2";
+
+//double equal means comparing the value only
+if(first==second){
+    console.log("Condition is true");
+}
+else{
+    console.log("condition is false")
+}
+
+//triple equal means comparing both the values and the types
+if(first===second){
+    console.log("Condition is true");
+}
+else{
+    console.log("condition is false")
+}
+
+//0 means false
+//1 means true
