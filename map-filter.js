998f982c5227c99d14913843b487e75a9c92046f
Piyal Biswas <biswaspiyal777@gmail.com>
2020-05-10 09:23:46 +0600

    map-filter-find

---
+++ map-filter.js
@@ -0,0 +1,29 @@
+const numbers = [1, 2, 3, 4, 5, 6, 7];
+const output = [];
+for (let i = 0; i < numbers.length; i++) {
+    const element = numbers[i];
+    const result = element * element;
+    output.push(result);
+}
+console.log(output);
+
+// function square(element){
+//     return element * element;
+// }
+
+// const square = element => element * element;
+
+// const square = x => x * x;
+
+// const result = numbers.map(function(element, index, array){
+//     return element * element;
+// })
+
+const result = numbers.map( x => x * x);
+console.log(result);
+
+const biggerThanFive = numbers.filter( x => x>5);
+console.log(biggerThanFive);
+
+const isThere = numbers.find( x => x<5);
+console.log(isThere);
