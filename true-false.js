f08a631b57755ff0cb615b6a6c667864531d17d5
Piyal Biswas <biswaspiyal777@gmail.com>
2020-05-10 07:46:01 +0600

    true vs false

---
+++ true-false.js
@@ -0,0 +1,19 @@
+const name="Salman";
+
+if(name.length>0){
+    console.log("Condition is true");
+}
+else{
+    console.log("Condition is false");
+}
+
+//for falsy:
+//0
+//""
+//undefined
+//null
+//NaN
+//false
+
+//truthy:
+//'0', ' ',[],object,'false'
