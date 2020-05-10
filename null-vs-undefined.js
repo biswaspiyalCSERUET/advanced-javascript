1b32dbbc98d36b9fa902760b31e54d5822931050
Piyal Biswas <biswaspiyal777@gmail.com>
2020-05-10 08:00:59 +0600

    null vs undefined,cleared for the interview

---
+++ null-vs-undefined.js
@@ -0,0 +1,21 @@
+let bird;
+
+function add(num1,num2){
+    console.log(num1+num2);
+    return
+}
+
+function add(num1,num2){
+    console.log(num1,num2);
+}
+
+const boy={name:"Rahim",phone:2345};
+console.log(boy.gf);
+
+let fun=undefined;
+console.log(fun);
+
+let ages=[12,13,11];
+console.log(ages[8]);
+
+// null=not existed
